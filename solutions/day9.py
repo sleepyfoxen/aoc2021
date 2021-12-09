ea01198023a23ce61742f0f94556644ee6199e2a
sleepyfox <fox@foxen.uk>
2021-12-09 12:46:18 +0000

    use format style that rly makes people feel something, yknow?
    i am in pain

--- solutions/day9.py
+++ solutions/day9.py
@@ -13,18 +13,16 @@ lowpoints = dict()
 
 for y, line in enumerate(in_):
     for x, c in enumerate(line):
-        minadj = 0
-
         # search for a lower point nearby
         for (y_, x_) in ((-1, 0), (1, 0), (0, -1), (0, 1)):
             y_, x_ = y + y_, x + x_
-            if y_ < 0 or x_ < 0 or y_ >= len(in_) or x_ >= len(in_[y]):
-                continue
+            if y_ < 0 or x_ < 0:                    continue
+            if y_ >= len(in_) or x_ >= len(in_[y]): continue
 
-            c_ = in_[y_][x_]
-            if c >= c_:  break
+            c_ = in_[y_][x_]  # safe: bound checked
+            if c >= c_: break
 
-        # not found
+        # lower point not found
         else: lowpoints[y, x] = c
 
 print(sum(i+1 for i in lowpoints.values()))
@@ -35,43 +33,34 @@ def g(y, x):
     nine = [ False ] * 4
     scale = 1
     while True:
-        spoken = False
         for i, (dy, dx) in enumerate(cyclers):
-            if nine[i]: continue
             dy, dx = dy*scale, dx*scale
             y_, x_ = y + dy, x + dx
-
-            if y_ < 0 or x_ < 0 or y_ >= len(in_) or x_ >= len(in_[y]):
-                continue
+            if nine[i] or (y_, x_) in gs:           continue
+            if y_ < 0 or x_ < 0:                    continue
+            if y_ >= len(in_) or x_ >= len(in_[y]): continue
 
             p = in_[y_][x_]
-            if p == 9:
-                nine[i] = True
-                continue
-
-            spoken = True
-            yield y_, x_
+            if p == 9:  nine[i] = True
+            else:       yield y_, x_
         
-        if not spoken: return
+        if True in nine: return
         scale += 1
 
 gs = dict()
-
 blen = []
 for y, x in lowpoints.keys():
-    basin, basin_last = set(), None
+    basin, basin_last = set(), 0
     basin.add((y, x))
     g_ = g(y, x)
+    gs[y, x] = g_
     for p in g_: basin.add(p)
 
-    while not basin == basin_last:
-        basin_last = basin.copy()
-        for (y_, x_) in basin_last:
-            try: g_ = gs[(y_, x_)]
-            except KeyError:
-                g_ = g(y_, x_)
-                gs[(y_, x_)] = g_
-
+    while not len(basin) == basin_last:
+        basin_last = len(basin)
+        for (y_, x_) in (p for p in frozenset(basin) if p not in gs):
+            g_ = g(y_, x_)
+            gs[y_, x_] = g_
             for p in g_: basin.add(p)
 
     blen.append(len(basin))
